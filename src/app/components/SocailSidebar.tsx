e70542691dd505641e8168fee239ceeb54dd3c51
pankajkumar7403 <pankajkumar740303@gmail.com>
2024-10-23 00:45:50 +0530

    added google sheet to contact form

--- src/app/components/SocailSidebar.tsx
+++ src/app/components/SocailSidebar.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 const SocialSidebar = () => {
   return (
     <div className="fixed top-1/2 right-0 transform -translate-y-1/2 z-10 flex flex-col space-y-4 p-2 bg-white shadow-lg">
-      <a href="https://wa.me/+91 8800202305" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/918800202305" target="_blank" rel="noopener noreferrer">
         <FaWhatsapp className="text-green-500 hover:text-green-600" size={30} />
       </a>
       <a href="https://www.facebook.com/your-profile" target="_blank" rel="noopener noreferrer">
@@ -14,7 +14,7 @@ const SocialSidebar = () => {
       <a href="https://www.instagram.com/your-profile" target="_blank" rel="noopener noreferrer">
         <FaInstagram className="text-pink-500 hover:text-pink-600" size={30} />
       </a>
-      <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/company/bytechnology-ind" target="_blank" rel="noopener noreferrer">
         <FaLinkedin className="text-blue-500 hover:text-blue-600" size={30} />
       </a>
     </div>
